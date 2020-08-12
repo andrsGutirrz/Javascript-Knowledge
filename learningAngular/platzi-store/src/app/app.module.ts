654ff3fb22d8f39bbe41215bf0583269e91e260a
Andres Gutierrez <andres.gutierrez.arcia@gmail.com>
2020-08-11 21:05:25 -0600

    lazy loading

--- learningAngular/platzi-store/src/app/app.module.ts
+++ learningAngular/platzi-store/src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ExponentialPipe } from './exponential.pipe';
 import { HighlightDirective } from './highlight.directive';
-import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BannerComponent } from './banner/banner.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProdcutDetailComponent } from './prodcut-detail/prodcut-detail.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
@@ -25,15 +23,14 @@ import { ProdcutDetailComponent } from './prodcut-detail/prodcut-detail.componen
     CartComponent,
     ExponentialPipe,
     HighlightDirective,
-    HomeComponent,
     ProductsComponent,
     ContactComponent,
     DemoComponent,
     HeaderComponent,
     FooterComponent,
-    BannerComponent,
     PageNotFoundComponent,
     ProdcutDetailComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
