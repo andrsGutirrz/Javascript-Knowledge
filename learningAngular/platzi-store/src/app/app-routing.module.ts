2ce126a01721f30bde02f1e3e14c223af990a9c9
Andres Gutierrez <andres.gutierrez.arcia@gmail.com>
2020-08-11 21:06:04 -0600

    lazy loading

--- learningAngular/platzi-store/src/app/app-routing.module.ts
+++ learningAngular/platzi-store/src/app/app-routing.module.ts
@@ -46,7 +46,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{
+  imports: [RouterModule.forRoot(routes, {
     preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
